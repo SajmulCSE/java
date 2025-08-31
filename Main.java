1bcfcb0b90af3c623130913033ff8c385991ad99
sajmul <abc@gmail.com>
2025-08-31 09:35:03 +0600

    lab quiz question

---
+++ Main.java
@@ -0,0 +1,14 @@
+ class Test {
+    int x;
+    Test() {x = 10;}
+    Test(int y) {x = y;}
+    }
+
+public class Main {
+    public static void main(String[]args) {
+Test t1 = new Test();
+Test t2 = new Test(20);
+
+System.out.println(t1.x + " "+ t2.x);
+    }
+}
--- Man.java
+++ Man.java
@@ -3,14 +3,14 @@ public class Man {
     int age;
     float height;
 
-    public Man() {}
-    
     public Man(String name, int age, float height) {
         this.name = name;
         this.age = age;
         this.height = height;
     }
-
+    
+    public Man() {}
+    
     public String getName() {
         return name;
     }
@@ -19,10 +19,8 @@ public class Man {
         return age;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[]rafinvaStrings) {
         Man man1 = new Man("Sajmul", 20, (float) 80.63);
-        Man man2 = new Man();
-        System.out.println(man2.hashCode());
         System.out.println(man1.getName());
         System.out.println(man1.getAge());
     }
